e364c3e49cea4a3d5893f0e1309f620307e5668f
Andrew Ramirez <andrewramirez@unomaha.edu>
2022-09-08 18:25:46 -0700

    Added Entities and Components.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,130 @@
-use rltk::{Rltk, GameState}; // Using amethyst roguelike toolkit (RLTK) https://github.com/amethyst/bracket-lib
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode}; // Using amethyst roguelike toolkit (RLTK) https://github.com/amethyst/bracket-lib
+use specs::prelude::*; // specs::prelude::World
+use std::cmp::{max, min};
+use specs_derive::*;
 
-struct State {}
+// From my basic data, derive boilerplate needed for component
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+#[derive(Component)]
+struct LeftMover {}
+struct State {
+    ecs: World, // Entity Component System Library 
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>, 
+                        WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty,pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        player_input(self, ctx);
+        self.run_systems();
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        
+        // Entities must have both pos and render to be displayed
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+
+    }
+}
+
+// Moves player 1x1 based of arrow key input
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79 , max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key { // Get user input
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
     }
 }
 
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Roguelike").build()?;
-    let gamestate = State {};
+    let mut gamestate = State {
+        ecs: World::new()
+    };
+
+    // Register Calls
+    gamestate.ecs.register::<Position>();
+    gamestate.ecs.register::<Renderable>();
+    gamestate.ecs.register::<LeftMover>();
+    gamestate.ecs.register::<Player>();
+
+    gamestate.ecs
+        .create_entity()
+        .with(Position { x: 40, y: 25 })
+        .with(Renderable {
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Player{})
+        .build();
+
+    for i in 0..10 {
+        gamestate.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('☺'),
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(LeftMover{})
+            .build();
+    }
     rltk::main_loop(context, gamestate)
 }
